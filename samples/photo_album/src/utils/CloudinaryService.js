5018db265beb41351d2351eca0bf70855b57f662
Charlie Kelly <charliekelly31415@gmail.com>
2019-08-15 12:21:38 -0400

    replace 'myphotoalbum' tag with 'en plein air'

--- samples/photo_album/src/utils/CloudinaryService.js
+++ samples/photo_album/src/utils/CloudinaryService.js
@@ -1,4 +1,5 @@
 import { Cloudinary as CoreCloudinary, Util } from 'cloudinary-core';
+import { ALBUM_TAG } from '../utils/Constants';
 
 export const url = (publicId, options) => {
     const scOptions = Util.withSnakeCaseKeys(options);
@@ -12,7 +13,7 @@ export const openUploadWidget = (options, callback) => {
 };
 
 export const fetchPhotos = cloudName => {
-    // instead of maintaining the list of images, we rely on the 'myphotoalbum' tag
+    // instead of maintaining the list of images, we rely on the 'en plein air' tag
     // and simply retrieve a list of all images with that tag.
     // the version property is used for cache bust (lists are cached by the CDN for 1 minute)
     // *************************************************************************
@@ -26,7 +27,7 @@ export const fetchPhotos = cloudName => {
         version: Math.ceil(new Date().getTime() / 1000),
     };
 
-    const urlPath = url('myphotoalbum', options);
+    const urlPath = url(ALBUM_TAG, options);
 
     return fetch(urlPath)
         .then(res => res.text())
